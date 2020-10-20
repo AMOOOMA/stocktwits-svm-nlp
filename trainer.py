2b374a76d8a3462b6e1e18b43533382a860fdbf7
RuiLong J <jrl-china@hotmail.com>
2020-10-20 10:38:03 +0800

    Add only labelled csv file

--- trainer.py
+++ trainer.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from collections import Counter
 
 import numpy as np
@@ -8,6 +9,14 @@ from sklearn.model_selection import KFold
 from sklearn.feature_extraction import DictVectorizer
 
 from nltk.stem import SnowballStemmer
+
+from enum import Enum
+
+
+class Label(Enum):
+    NO_LABEL = "NO_LABEL"
+    NEG_LABEL = "Bearish"
+    POS_LABEL = "Bullish"
 
 
 def tokenize(message):
@@ -36,17 +45,20 @@ class Trainer:
 
 
 def main():
-    with open('CS 490a hw2 final dataset - Sheet1.csv', encoding="utf8") as csv_file:
+    path = "./stocktwits_labelled.csv"
+    with open(path, 'r', encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
-        class_dict = {
-            "pos": [],
-            "neg": [],
-            "neu": [],
+        data = {
+            Label.NEG_LABEL.value: [],
+            Label.POS_LABEL.value: [],
         }
 
-        for row in reader:
-            if row[0] != "Data entries":
-                class_dict[row[1]].append(row[0])
+        for label, msg in reader:
+            data[label].append(msg)
+
+        print("The NEG class' messages count: ", len(data[Label.NEG_LABEL.value]))
+        print("The POS class' messages count: ", len(data[Label.POS_LABEL.value]))
+
 
 
 if __name__ == "__main__":

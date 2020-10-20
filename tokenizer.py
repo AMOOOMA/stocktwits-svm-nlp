8c1e1c0ef6417bc374c4e62bebc27a760f96dc94
RuiLong J <jrl-china@hotmail.com>
2020-10-20 14:18:33 +0800

    Update instructions

--- tokenizer.py
+++ tokenizer.py
@@ -4,28 +4,47 @@ import twokenize
 class Tokenizer:
 
     def __init__(self):
-        self.message = []
+        self.message = ""
         self.cash_tags = []
 
     def tokenize(self, message):
+        """
+        # Tokenize cleans the @ symbols and
+        # extract the cash tag in the message
+        # Then the message is pass into
+        # tweetmotif's tokenizer for processing
+        # returns a tuple: ([cash_tags], [tokens])
+        """
         self.message = message
         self._clean_at_symbols()
-        self._replace_emojis()
         self._extract_cash_tags()
-        return twokenize(self.message)  # calls the Tweetmotif's tokenizer (https://github.com/brendano/tweetmotif)
+        return self.cash_tags, twokenize(
+            self.message)  # calls the Tweetmotif's tokenizer (https://github.com/brendano/tweetmotif)
 
     def _clean_at_symbols(self):
+        """
+        # Should remove all @token in message
+        # replace the message in self with
+        # the new cleaned string
+        """
         self.message = ""
 
     def _extract_cash_tags(self):
+        """
+        # Extracts the $company in message
+        # and update the list of cash_tags
+        # Note, the message in self should
+        # be updated with the cleaned string
+        """
         self.cash_tags = []
-
-    def _replace_emojis(self):
         self.message = ""
 
 
 def main():
-    return
+    # for testing propose
+    tokenizer = Tokenizer()
+    cash_tags, tokens = tokenizer.tokenize("$AAPL, $JD Hello this is a @someone test message $AMZN")
+    print(cash_tags, tokens)
 
 
 if __name__ == "__main__":

efc1e6c44d6a6071062fe7fb3459b449f3eb49a7
RuiLong J <jrl-china@hotmail.com>
2020-10-20 14:25:30 +0800

    Update to use twokenize first

--- tokenizer.py
+++ tokenizer.py
@@ -4,46 +4,47 @@ import twokenize
 class Tokenizer:
 
     def __init__(self):
-        self.message = ""
+        self.message = []
         self.cash_tags = []
 
     def tokenize(self, message):
         """
-        # Tokenize cleans the @ symbols and
-        # extract the cash tag in the message
-        # Then the message is pass into
+        # First the message is pass into
         # tweetmotif's tokenizer for processing
+        # https://github.com/brendano/tweetmotif
+        # Then tokenize cleans the @ symbols and
+        # extract the cash tag in the message
         # returns a tuple: ([cash_tags], [tokens])
         """
-        self.message = message
+        self.message = twokenize.tokenize(message)  # calls the Tweetmotif's tokenizer
         self._clean_at_symbols()
         self._extract_cash_tags()
-        return self.cash_tags, twokenize(
-            self.message)  # calls the Tweetmotif's tokenizer (https://github.com/brendano/tweetmotif)
+        return self.cash_tags, self.message
 
     def _clean_at_symbols(self):
         """
-        # Should remove all @token in message
+        # Should remove all @token in tokens
         # replace the message in self with
-        # the new cleaned string
+        # the cleaned list of tokens
         """
-        self.message = ""
+        self.message = []
 
     def _extract_cash_tags(self):
         """
-        # Extracts the $company in message
+        # Extracts the $company in tokens
         # and update the list of cash_tags
-        # Note, the message in self should
-        # be updated with the cleaned string
+        # Note, the tokens in self should
+        # be updated with new tokens list
         """
         self.cash_tags = []
-        self.message = ""
+        self.message = []
 
 
 def main():
     # for testing propose
     tokenizer = Tokenizer()
-    cash_tags, tokens = tokenizer.tokenize("$AAPL, $JD Hello this is a @someone test message $AMZN")
+    test_message = "$AAPL, $JD Hello this is a @someone https://github.com/AMOOOMA/stocktwits-svm-nlp test message $AMZN"
+    cash_tags, tokens = tokenizer.tokenize(test_message)
     print(cash_tags, tokens)
 
 

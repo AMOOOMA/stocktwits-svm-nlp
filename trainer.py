664f2b59afc9b4591e02db2f25b1b53ea68312b6
RuiLong J <jrl-china@hotmail.com>
2020-10-23 15:43:33 +0800

    Add NB model and code refractor

--- trainer.py
+++ trainer.py
@@ -7,6 +7,7 @@ from tokenizer import Tokenizer
 import numpy as np
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import KFold
 from sklearn.feature_extraction import DictVectorizer
 
@@ -53,20 +54,12 @@ def find_index(token, low, high, features):  # binary search to find element ind
 class Trainer:
 
     def __init__(self, data):
-        # common variables
         self.data = data
         self.cash_tags = []
-
-        # variables for baseline algo
         self.bow = Counter()
         self.bow_features = []
-        self.log_reg_X = []
-        self.log_reg_y = []
-
-        # variables for SVM
-        self.features = []
-        self.X = []
-        self.y = []
+        self.bow_X = []
+        self.bow_y = []
 
     def _fill_bow(self):  # fill the bow dictionary with self.data
         for label in self.data:
@@ -82,7 +75,7 @@ class Trainer:
         self.bow_features = vec.get_feature_names()
         self.bow_features.sort()  # prepare for binary search
 
-    def _generate_bow_X(self):
+    def _generate_bow_dataset(self):
         # key = (NEG_LABEL, POS_LABEL)
         for key in self.data:
             for message in self.data[key]:
@@ -94,34 +87,46 @@ class Trainer:
                     line_vector[idx] += 1
 
                 line_vector[len(line_vector) - 1] = len(tokens)
-                self.log_reg_X.append(line_vector)
+                self.bow_X.append(line_vector)
 
-    def _bow_train(self):
-        # Format into numpy data structure
-        self.log_reg_y = np.concatenate(([Label.NEG_LABEL.value] * len(self.data[Label.NEG_LABEL.value]),
-                                         [Label.POS_LABEL.value] * len(self.data[Label.POS_LABEL.value])))
-        self.log_reg_X = np.array(self.log_reg_X)
+        self.bow_y = np.concatenate(([Label.NEG_LABEL.value] * len(self.data[Label.NEG_LABEL.value]),
+                                     [Label.POS_LABEL.value] * len(self.data[Label.POS_LABEL.value])))
+
+        # Format into numpy data structure just in case
+        self.bow_X = np.array(self.bow_X)
+        self.bow_y = np.array(self.bow_y)
+
+    def _bow_train(self, model):
+        # Make copies because of reuse
+        X = self.bow_X.copy()
+        y = self.bow_y.copy()
 
         # Creates model and cross validation sets
-        kf = KFold(n_splits=10, shuffle=True)
+        kf = KFold(n_splits=5, shuffle=True)
         kf.get_n_splits()
-        model = LogisticRegression(solver='sag', random_state=0, n_jobs=-1, verbose=10)
         model_score = []
 
-        for train_index, test_index in kf.split(self.log_reg_X, self.log_reg_y):
-            X_train, X_test = self.log_reg_X[train_index], self.log_reg_X[test_index]
-            y_train, y_test = self.log_reg_y[train_index], self.log_reg_y[test_index]
+        for train_index, test_index in kf.split(X, y):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
             model.fit(X_train, y_train)
             model_score.append(model.score(X_test, y_test))
 
-        return model, model_score
+        return model_score
 
-    def get_bow_score(self):
+    def print_bow_score(self):
         self._fill_bow()
         self._generate_bow_feature_vector()
-        self._generate_bow_X()
-        model, scores = self._bow_train()
-        return sum(scores) / len(scores)
+        self._generate_bow_dataset()
+
+        log_reg_model = LogisticRegression(solver='sag', random_state=0, n_jobs=-1, verbose=10)
+        log_reg_scores = self._bow_train(log_reg_model)
+
+        naive_bayes_model = GaussianNB()
+        naive_bayes_scores = self._bow_train(naive_bayes_model)
+
+        print("Log Reg score: ", sum(log_reg_scores) / len(log_reg_scores))
+        print("Naive Bayes score: ", sum(naive_bayes_scores) / len(naive_bayes_scores))
 
 
 def main():
@@ -140,7 +145,7 @@ def main():
         print("The POS class' messages count: ", len(data[Label.POS_LABEL.value]))
 
         trainer = Trainer(data)
-        print(trainer.get_bow_score())
+        trainer.print_bow_score()
 
 
 if __name__ == "__main__":

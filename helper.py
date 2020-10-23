1a1d3c6e97f9e54d020c9cd7070a53a404819d1f
RuiLong J <jrl-china@hotmail.com>
2020-10-23 16:03:56 +0800

    Major file structure refractor

---
+++ helper.py
@@ -0,0 +1,40 @@
+from tokenizer import Tokenizer
+
+from nltk.stem import SnowballStemmer
+
+from enum import Enum
+
+
+class Label(Enum):
+    NO_LABEL = "NO_LABEL"
+    NEG_LABEL = "Bearish"
+    POS_LABEL = "Bullish"
+
+
+def tokenize(message):
+    """
+    return (cash_tags : [string], tokens : [string])
+        tuple containing cash_tags and tokens
+    """
+    custom_tokenizer = Tokenizer()
+    return custom_tokenizer.tokenize(message)
+
+
+def process_tokens(tokens):
+    stemmer = SnowballStemmer("english")
+    tokens = map(lambda x: stemmer.stem(x), tokens)
+    return list(tokens)
+
+
+def find_index(token, low, high, features):  # binary search to find element index in list
+    if high >= low:
+        mid = int((high + low) / 2)
+
+        if features[mid] == token:
+            return mid
+        elif features[mid] > token:
+            return find_index(token, low, mid - 1, features)
+        else:
+            return find_index(token, mid + 1, high, features)
+
+    return -1

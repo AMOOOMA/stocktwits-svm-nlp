c4556bfc2d7cbe28e63759ee4559656580edfafb
RuiLong J <jrl-china@hotmail.com>
2020-10-24 20:39:14 +0800

    Add update BERT compatible tokenize

--- bert_word_embedding.py
+++ bert_word_embedding.py
@@ -13,16 +13,45 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 class BertWordEmbedding:
 
     def __init__(self):
-        self.tokens = ""
+        self.tokens = []
+
+    def _update_tokens(self, token_ids):
+        self.tokens = []  # clear list first
+        vocab = list(tokenizer.vocab.keys())
+        for token_id in token_ids:
+            self.tokens.append(vocab[token_id])
 
     def get_message_embedding(self, message):
-        cash_tags, self.tokens = tokenize(message)  # use our custom tokenizer
-        encoded = tokenizer.encode_plus(
+        _, self.tokens = tokenize(message)  # use our custom tokenizer
+        encoded = tokenizer.encode_plus(  # encoded the pre tokenized message
             text=self.tokens,
             add_special_tokens=True,
             is_split_into_words=True
-        )  # encoded the pre tokenized message
-        print(encoded.tokens())
+        )
+        self._update_tokens(encoded['input_ids'])  # update with BERT compatible tokens list
+        print(self.tokens)
+
+        input_ids_tensor = torch.tensor([encoded['input_ids']])
+        attention_mask_tensors = torch.tensor([encoded['attention_mask']])
+
+        model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+        model.eval()
+
+        with torch.no_grad():
+
+            outputs = model(input_ids_tensor, attention_mask_tensors)
+            hidden_states = outputs[2]
+
+            print("Number of layers:", len(hidden_states), "  (initial embeddings + 12 BERT layers)")
+            layer_i = 0
+
+            print("Number of batches:", len(hidden_states[layer_i]))
+            batch_i = 0
+
+            print("Number of tokens:", len(hidden_states[layer_i][batch_i]))
+            token_i = 0
+
+            print("Number of hidden units:", len(hidden_states[layer_i][batch_i][token_i]))
 
         return self.tokens
 

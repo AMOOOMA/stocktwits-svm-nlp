fb8e15adb9b242b0d76fb1ba4bc7b0019fd71a4e
RuiLong J <jrl-china@hotmail.com>
2020-10-25 22:00:57 +0800

    Update helper comments

--- helper.py
+++ helper.py
@@ -13,20 +13,31 @@ class Label(Enum):
 
 def tokenize(message):
     """
-    return (cash_tags : [string], tokens : [string])
-        tuple containing cash_tags and tokens
+    # return (cash_tags : [string], tokens : [string])
+    #   tuple containing cash_tags and tokens
     """
     custom_tokenizer = Tokenizer()
     return custom_tokenizer.tokenize(message)
 
 
 def process_tokens(tokens):
+    """
+    # Process the tokens with
+    # custom funcs/library
+    # Only stemmer for our project
+    """
     stemmer = SnowballStemmer("english")
     tokens = map(lambda x: stemmer.stem(x), tokens)
     return list(tokens)
 
 
 def find_index(token, low, high, features):  # binary search to find element index in list
+    """
+    # Binary search helper method
+    # helps finding token index
+    # with O(log n) time whereas
+    # np.where() will need O(n)
+    """
     if high >= low:
         mid = int((high + low) / 2)
 
@@ -38,3 +49,14 @@ def find_index(token, low, high, features):  # binary search to find element ind
             return find_index(token, mid + 1, high, features)
 
     return -1
+
+
+def PCA_reduce_dimensionality(X):
+    """
+    # Perform a PCA analysis
+    # on the dataset X and
+    # returns new X with reduced
+    # dimensionality for faster
+    # training time
+    """
+    return X

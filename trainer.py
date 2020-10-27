b1370c9c9848b095bbe5a6f8e504bb3408c293e0
RuiLong J <jrl-china@hotmail.com>
2020-10-27 10:27:58 +0800

    Trainer load embeddings

--- trainer.py
+++ trainer.py
@@ -1,8 +1,10 @@
 import csv
+import sys
 
 from collections import Counter
 
 import numpy as np
+import pandas as pd
 
 from sklearn.model_selection import KFold
 from sklearn.feature_extraction import DictVectorizer
@@ -22,21 +24,20 @@ class Trainer:
 
 
 def main():
-    path = "data/stocktwits_labelled_train.csv"
-    with open(path, 'r', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        data = {
-            Label.NEG_LABEL.value: [],
-            Label.POS_LABEL.value: [],
-        }
+    path = "../stocktwits_embeddings.csv"
+    reader = pd.read_csv(path, header=None)
+    data = {
+        Label.NEG_LABEL.value: [],
+        Label.POS_LABEL.value: [],
+    }
 
-        for label, message in reader:
-            data[label].append(message)
+    for index, row in reader.iterrows():
+        data[row[0]].append(row[1])
 
-        print("The NEG class' messages count: ", len(data[Label.NEG_LABEL.value]))
-        print("The POS class' messages count: ", len(data[Label.POS_LABEL.value]))
+    print("The NEG class' messages count: ", len(data[Label.NEG_LABEL.value]))
+    print("The POS class' messages count: ", len(data[Label.POS_LABEL.value]))
 
-        trainer = Trainer(data)
+    trainer = Trainer(data)
 
 
 if __name__ == "__main__":

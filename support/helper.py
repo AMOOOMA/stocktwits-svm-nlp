39a7cd502b201ccebfd53deaf93d1e6fc19adb72
RuiLong J <jrl-china@hotmail.com>
2020-11-12 15:35:59 +0800

    Add bert tokens csv

--- support/helper.py
+++ support/helper.py
@@ -61,12 +61,12 @@ def PCA_reduce_dimensionality(X):
     """
     return X
 
-###########################################################################################################
+########################################################################################################################
 #  Function archived, used when needed.
-
+#
 # import csv
 # from bert_word_embedding import BertWordEmbedding
-
+#
 # def generate_embedding_for_path(path):
 #     """
 #     # Helper for generate word embeddings
@@ -100,3 +100,70 @@ def PCA_reduce_dimensionality(X):
 #
 #         print("The NEG class' messages count: ", len(data[Label.NEG_LABEL.value]))
 #         print("The POS class' messages count: ", len(data[Label.POS_LABEL.value]))
+#
+########################################################################################################################
+# Used for generating standardize data, archived for backup
+#
+# def generate_standardize_data():
+#     path = "../stocktwits_embeddings.csv"
+#     reader = pd.read_csv(path, header=None)
+#     data = {
+#         Label.NEG_LABEL.value: [],
+#         Label.POS_LABEL.value: [],
+#     }
+#
+#     for index, row in reader.iterrows():
+#         # if str(row[1]) != "nan":
+#             data[row[0]].append(row[1])  # eval(str(message)) to convert data from string to list form
+#
+#     print("The NEG class' messages count: ", len(data[Label.NEG_LABEL.value]))
+#     print("The POS class' messages count: ", len(data[Label.POS_LABEL.value]))
+#
+#     token_data = {
+#         Label.NEG_LABEL.value: [],
+#         Label.POS_LABEL.value: [],
+#     }
+#
+#     vocab = list(tokenizer.vocab.keys())
+#
+#     with open("./data/stocktwits_labelled_train.csv", 'r', encoding='utf-8') as csv_file:
+#         reader = csv.reader(csv_file, delimiter=',')
+#
+#         for label, msg in reader:
+#             _, bert_tokens = tokenize(msg)
+#             if len(bert_tokens) == 0:
+#                 token_data[label].append(None)
+#                 continue
+#
+#             encoded = tokenizer.encode_plus(  # encoded the pre tokenized message
+#                 text=bert_tokens,
+#                 add_special_tokens=True,
+#                 is_split_into_words=True
+#             )
+#             token_data[label].append(list(map(lambda x: vocab[x], encoded['input_ids'])))
+#
+#     print("The NEG class' messages count: ", len(token_data[Label.NEG_LABEL.value]))
+#     print("The POS class' messages count: ", len(token_data[Label.POS_LABEL.value]))
+#
+#     with open("./data/stocktwits_labelled_train_bert_standardized.csv", 'w', newline='', encoding='utf-8') as csv_file:
+#         csv_file.truncate()  # clear the original file and rewrite
+#         writer = csv.writer(csv_file)
+#
+#         with open("./data/tokens.csv", 'w', newline='', encoding='utf-8') as token_csv_file:
+#             token_csv_file.truncate()
+#             token_writer = csv.writer(token_csv_file)
+#
+#             for label in data.keys():
+#                 for i in range(len(data[label])):
+#                     if token_data[label][i] is None or str(data[label][i]) == "nan":
+#                         continue
+#                     tokens = token_data[label][i][1:-1]
+#                     embeddings = eval(str(data[label][i]))[1:-1]
+#                     if len(tokens) == len(embeddings):
+#                         tokens, embeddings = standardize_by_duplicating(tokens, embeddings, 10)
+#                         print(tokens, i)
+#                         writer.writerow([label, embeddings])
+#                         token_writer.writerow([label, tokens])
+#                     else:
+#                         print(len(tokens), len(embeddings))
+#                         print("Something is wrong!!!!\n")

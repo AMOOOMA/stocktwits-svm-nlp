cc4e92e5eac16fa5c3b62024fffae4787f031521
RuiLong J <jrl-china@hotmail.com>
2020-11-17 15:28:23 +0800

    Update formatting

--- fine_tune_bert.py
+++ fine_tune_bert.py
@@ -24,7 +24,6 @@ epochs = 4
 seed_val = 42
 
 if torch.cuda.is_available():
-
     device = torch.device("cuda")
     print('There are %d GPU(s) available.' % torch.cuda.device_count())
     print('We will use the GPU:', torch.cuda.get_device_name(0))
